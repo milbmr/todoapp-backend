e6a92c8a5249105d77e1a688dd82c03b0ebdfae3
miloud <miloamg@gmail.com>
2023-07-20 20:14:48 +0100

    added refresh token

--- Models/TodoUser.cs
+++ Models/TodoUser.cs
@@ -5,5 +5,7 @@ namespace Backend.Models
     public class TodoUser : IdentityUser
     {
         public ICollection<TodoItem> TodoItems { get; set; } = null!;
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
     }
 }
