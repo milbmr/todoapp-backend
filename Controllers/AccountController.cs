77698ef418fd1b9313b52e04a3534d4f3b531dc2
miloud <miloamg@gmail.com>
2023-07-20 22:28:15 +0100

    added token endpoints

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -10,172 +10,225 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Backend.DTO;
+using Microsoft.AspNetCore.Authorization;
 
-namespace Backend.Controllers
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class AccountController : Controller
 {
-    [ApiController]
-    [Route("api/[controller]/[action]")]
-    public class AccountController : Controller
+    private readonly TodoContext context;
+    private readonly ILogger<TodoItemsController> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly UserManager<TodoUser> _userManager;
+
+    private readonly SignInManager<TodoUser> _signInManager;
+
+    public AccountController(
+        ILogger<TodoItemsController> logger,
+        TodoContext context,
+        IConfiguration configuration,
+        UserManager<TodoUser> userManager,
+        SignInManager<TodoUser> signInManager
+    )
     {
-        private readonly TodoContext context;
-        private readonly ILogger<TodoItemsController> _logger;
-        private readonly IConfiguration _configuration;
-        private readonly UserManager<TodoUser> _userManager;
+        this.context = context;
+        _configuration = configuration;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _logger = logger;
+    }
 
-        private readonly SignInManager<TodoUser> _signInManager;
-
-        public AccountController(ILogger<TodoItemsController> logger, TodoContext context,
-            IConfiguration configuration,
-            UserManager<TodoUser> userManager,
-            SignInManager<TodoUser> signInManager)
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterDTO input)
+    {
+        try
         {
-            this.context = context;
-            _configuration = configuration;
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> Register(RegisterDTO input)
-        {
-            try
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                var newUser = new TodoUser();
+                newUser.UserName = input.UserName;
+                newUser.Email = input.Email;
+                var result = await _userManager.CreateAsync(newUser, input.Password!);
+                if (result.Succeeded)
                 {
-                    var newUser = new TodoUser();
-                    newUser.UserName = input.UserName;
-                    newUser.Email = input.Email;
-                    var result = await _userManager.CreateAsync(
-                        newUser, input.Password!);
-                    if (result.Succeeded)
-                    {
-                        _logger.LogInformation(
-                            "User {userName} ({email}) has been created.",
-                            newUser.UserName, newUser.Email);
-                        return StatusCode(201,
-                            $"User '{newUser.UserName}' has been created.");
-                    }
-                    else
-                        throw new Exception(
-                            string.Format("Error: {0}",
-                            string.Join(" ", result.Errors.Select(e => e.Description))));
+                    _logger.LogInformation(
+                        "User {userName} ({email}) has been created.",
+                        newUser.UserName,
+                        newUser.Email
+                    );
+                    return StatusCode(201, $"User '{newUser.UserName}' has been created.");
                 }
                 else
-                {
-                    var details = new ValidationProblemDetails(ModelState);
-                    details.Type =
-                        "https://tools.ietf.org/html/rfc7231#section-6.5.1";
-                    details.Status = StatusCodes.Status400BadRequest;
-                    return new BadRequestObjectResult(details);
-                }
+                    throw new Exception(
+                        string.Format(
+                            "Error: {0}",
+                            string.Join(" ", result.Errors.Select(e => e.Description))
+                        )
+                    );
             }
-            catch (Exception e)
+            else
             {
-                var exceptionDetails = new ProblemDetails();
-                exceptionDetails.Detail = e.Message;
-                exceptionDetails.Status =
-                    StatusCodes.Status500InternalServerError;
-                exceptionDetails.Type =
-                    "https://tools.ietf.org/html/rfc7231#section-6.6.1";
-                return StatusCode(
-                    StatusCodes.Status500InternalServerError,
-                    exceptionDetails);
+                var details = new ValidationProblemDetails(ModelState);
+                details.Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                details.Status = StatusCodes.Status400BadRequest;
+                return new BadRequestObjectResult(details);
             }
         }
-
-        [HttpPost]
-        public async Task<ActionResult> Login(LoginDTO input)
+        catch (Exception e)
         {
-            try
+            var exceptionDetails = new ProblemDetails();
+            exceptionDetails.Detail = e.Message;
+            exceptionDetails.Status = StatusCodes.Status500InternalServerError;
+            exceptionDetails.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+            return StatusCode(StatusCodes.Status500InternalServerError, exceptionDetails);
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginDTO input)
+    {
+        try
+        {
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
-                {
-                    var user = await _userManager.FindByNameAsync(input.UserName!);
-                    if (user == null ||
-                    !await _userManager.CheckPasswordAsync(user, input.Password!))
-                        throw new Exception("invalid login attempt");
-
-                    else
-                    {
-                        var signingCredentials = CreateSigningCredentials();
-
-                        var claims = CreateClaims(user);
-
-                        var jwtObject = new JwtSecurityToken(
-                            issuer: _configuration["JWT:Issuer"],
-                            audience: _configuration["JWT:Audience"],
-                            claims: claims,
-                            expires: DateTime.Now.AddSeconds(300),
-                            signingCredentials: signingCredentials
-                        );
-
-                        var accessToken = new JwtSecurityTokenHandler().WriteToken(jwtObject);
-                        var refreshToken = GenerateRefreshToken();
-
-                        user.RefreshToken = refreshToken;
-                        user.RefreshTokenExpiryTime = DateTime.Now.AddSeconds(20);
-
-                        await _userManager.UpdateAsync(user);
-
-                        Response.Cookies.Append("refresh-token", refreshToken, new CookieOptions()
-                        {
-                            HttpOnly = true,
-                            SameSite = SameSiteMode.None
-                        });
-
-                        return StatusCode(StatusCodes.Status200OK, accessToken);
-                    }
-
-                }
+                var user = await _userManager.FindByNameAsync(input.UserName!);
+                if (user == null || !await _userManager.CheckPasswordAsync(user, input.Password!))
+                    throw new Exception("invalid login attempt");
                 else
                 {
-                    Console.WriteLine("in auth");
-                    var details = new ValidationProblemDetails(ModelState);
-                    details.Status = StatusCodes.Status400BadRequest;
-                    return new BadRequestObjectResult(details);
+                    var accessToken = GenerateAccessToken(user);
+                    var refreshToken = GenerateRefreshToken();
+
+                    user.RefreshToken = refreshToken;
+                    user.RefreshTokenExpiryTime = DateTime.Now.AddSeconds(20);
+
+                    await _userManager.UpdateAsync(user);
+
+                    Response.Cookies.Append(
+                        "refresh-token",
+                        refreshToken,
+                        new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None }
+                    );
+
+                    return StatusCode(
+                        StatusCodes.Status200OK,
+                        new { accessToken, user = user.UserName }
+                    );
                 }
             }
-            catch (Exception e)
+            else
             {
-                var exceptionDetails = new ProblemDetails();
-                exceptionDetails.Detail = e.Message;
-                exceptionDetails.Status = StatusCodes.Status401Unauthorized;
-                return StatusCode(
-                    StatusCodes.Status401Unauthorized, exceptionDetails
-                );
+                Console.WriteLine("in auth");
+                var details = new ValidationProblemDetails(ModelState);
+                details.Status = StatusCodes.Status400BadRequest;
+                return new BadRequestObjectResult(details);
             }
         }
-
-        private SigningCredentials CreateSigningCredentials()
+        catch (Exception e)
         {
-            return new SigningCredentials(
-                            new SymmetricSecurityKey(
-                                System.Text.Encoding.UTF8.GetBytes(
-                                    _configuration["JWT:SigningKey"]!
-                                )
-                            ),
-                            SecurityAlgorithms.HmacSha256
-                        );
-        }
-
-        private List<Claim> CreateClaims(IdentityUser user)
-        {
-            List<Claim> claims = new List<Claim>() {
-                    new Claim(ClaimTypes.Name, user.UserName!)
-                };
-
-            return claims;
-        }
-
-        private string GenerateRefreshToken()
-        {
-            var randomNumber = new byte[32];
-            using (var numGen = RandomNumberGenerator.Create())
+            var exceptionDetails = new ProblemDetails()
             {
-                numGen.GetBytes(randomNumber);
-                return Convert.ToBase64String(randomNumber);
-            }
+                Detail = e.Message,
+                Status = StatusCodes.Status401Unauthorized
+            };
+            return StatusCode(StatusCodes.Status401Unauthorized, exceptionDetails);
         }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Refresh()
+    {
+        var refreshToken = "";
+        if (
+            !Request.Headers.TryGetValue("user", out var userName)
+            && !Request.Cookies.TryGetValue("refresh-token", out refreshToken)
+        )
+            BadRequest();
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+
+        if (
+            user == null
+            || user.RefreshToken != refreshToken
+            || user.RefreshTokenExpiryTime <= DateTime.Now
+        )
+            BadRequest("invalid refresh request");
+
+        var newAccessToken = GenerateAccessToken(user!);
+        var newRefreshToken = GenerateRefreshToken();
+
+        user!.RefreshToken = newRefreshToken;
+
+        await _userManager.UpdateAsync(user);
+
+        Response.Cookies.Append(
+            "refresh-token",
+            newAccessToken,
+            new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None }
+        );
+
+        return Ok(new { accessToken = newAccessToken, userName = user.UserName });
+    }
+
+    [HttpPost, Authorize]
+    public async Task<IActionResult> Revoke()
+    {
+        if (!Request.Headers.TryGetValue("user", out var userName))
+            BadRequest();
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+
+        if (user == null)
+            BadRequest();
+
+        user!.RefreshToken = null;
+        await _userManager.UpdateAsync(user);
+
+        return NoContent();
+    }
+
+    private SigningCredentials CreateSigningCredentials()
+    {
+        return new SigningCredentials(
+            new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(_configuration["JWT:SigningKey"]!)
+            ),
+            SecurityAlgorithms.HmacSha256
+        );
+    }
+
+    private List<Claim> CreateClaims(IdentityUser user)
+    {
+        List<Claim> claims = new() { new Claim(ClaimTypes.Name, user.UserName!) };
+
+        return claims;
+    }
+
+    private string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var numGen = RandomNumberGenerator.Create();
+        numGen.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    private string GenerateAccessToken(IdentityUser user)
+    {
+        var signingCredentials = CreateSigningCredentials();
+
+        var claims = CreateClaims(user);
+
+        var jwtObject = new JwtSecurityToken(
+            issuer: _configuration["JWT:Issuer"],
+            audience: _configuration["JWT:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddSeconds(300),
+            signingCredentials: signingCredentials
+        );
+
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(jwtObject);
+        return accessToken;
     }
 }
